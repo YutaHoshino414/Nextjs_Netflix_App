5d6c80484df5645778047c1351611949c3bcb610
USER <y.hoshino25@gmail.com>
2022-06-28 21:08:13 +0900

    start with creating a header component

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+        </ul>
+        </div>
+        <div></div>
+    </header>
+  )
+}
