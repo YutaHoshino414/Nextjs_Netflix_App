ec8e5dcf0100ecf29fc6a299db6dc1a9be5cad97
USER <y.hoshino25@gmail.com>
2022-06-29 20:27:12 +0900

    Add header component with styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,37 @@
 import React from 'react'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header>
-        <div>
+        <div className='flex items-center space-x-2'>
         <img
           src="https://rb.gy/ulxxee"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
         />
-        <ul>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
+        <ul className='hidden space-x-4 md:flex'>
+          <li className='headerLink'>Home</li>
+          <li className='headerLink'>TV Shows</li>
+          <li className='headerLink'>Movies</li>
+          <li className='headerLink'>New & Popular</li>
+          <li className='headerLink'>My List</li>
         </ul>
         </div>
-        <div></div>
+        <div className="flex items-center space-x-4 text-sm font-light">
+          <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+          <p className="hidden lg:inline">Kids</p>
+          <BellIcon className="h-6 w-6" />
+          <Link href="/account">
+            <img
+              src="https://rb.gy/g1pwyx"
+              alt=""
+              className="cursor-pointer rounded"
+            />
+          </Link>
+        </div>
     </header>
   )
 }
